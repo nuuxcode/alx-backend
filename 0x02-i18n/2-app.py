1d79aa667bdc5fb0c02a8d6a64f27973b77c3213
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-06 23:57:59 +0100

    😏🙄😶 Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ doc doc doc """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -17,10 +17,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """doc doc doc"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route("/")
 def index():
     """doc doc doc"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
